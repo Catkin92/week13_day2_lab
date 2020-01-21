a28ba943df983aa7ede97f5e3e425c7a27094ee5
Catrin <catrinhughes92@gmail.com>
2020-01-21 10:59:47 +0000

    got employee by id to work

--- src/main/java/com/codeclan/example/MorningLab/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/MorningLab/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.codeclan.example.MorningLab.models.Employee;
 import com.codeclan.example.MorningLab.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class EmployeeController {
     }
 
     @GetMapping("{id}")
-    public Optional<Employee> getEmployeeById(long id) {
+    public Optional<Employee> getEmployeeById(@PathVariable long id) {
         return employeeRepository.findById(id);
     }
 }
