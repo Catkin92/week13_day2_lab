d6aea1d08cc9861a9cba9486d8d0db6af9bbf1f7
Catrin <catrinhughes92@gmail.com>
2020-01-21 09:46:11 +0000

    added stuff to application properties

---
+++ src/main/java/com/codeclan/example/MorningLab/MorningLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.MorningLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorningLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorningLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/MorningLab/MorningLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.MorningLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorningLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
